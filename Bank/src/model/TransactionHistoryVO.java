790865c2b685bf30628ba8ad13d566ae3c069213
HEEJIN LIM <xeexin0331@gmail.com>
2025-04-29 17:24:19 +0900

    chore : 오타 수정

--- Bank/src/model/TransactionHistoryVO.java
+++ Bank/src/model/TransactionHistoryVO.java
@@ -7,7 +7,7 @@ public class TransactionHistoryVO {
 	private int transactionId;// 거래ID
 	private String accountNo;// 계좌번호
 	private String transactionTime;// 거래종류(입금/출금/이체)
-	private BigDecimal ammount;// 거래금액
+	private BigDecimal amount;// 거래금액
 	private LocalDateTime transactionDate;// 거래일시
 	private String targetAccount; // 이체할 상대 계좌
 
